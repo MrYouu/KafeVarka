b6d32d589efe7f8bf1137ad60e91a17bac4df683
Jim <51420306+MrYouu@users.noreply.github.com>
2020-06-22 15:10:12 +0300

    Add files via upload

--- Files/functions.js
+++ Files/functions.js
@@ -44,6 +44,113 @@ function closeSideMenu()
     }
 }
 
+function selectRandomDrinkOpenMenu()
+{
+    document.getElementById("selectedDrink").style.display = "unset"
+
+    var randomDrink = Math.floor(Math.random() * (16 - 1)) + 1;
+
+    var Drink = document.getElementById("Drink");
+    var Name = document.getElementById("Name");
+    var Quantity = document.getElementById("Quantity");
+
+    if (randomDrink == 1)
+    {
+        Drink.src = "../Images/Drinks/aisShokolad.png";
+        Name.innerHTML = "Айс шоколад";
+        Quantity.innerHTML = "300мл";
+    }
+    else if (randomDrink == 2)
+    {
+        Drink.src = "../Images/Drinks/Chai.png";
+        Name.innerHTML = "Чай";
+        Quantity.innerHTML = "500мл";
+    }
+    else if (randomDrink == 3)
+    {
+        Drink.src = "../Images/Drinks/dvoinoKapuchino.png";
+        Name.innerHTML = "Двойно капучино";
+        Quantity.innerHTML = "500мл";
+    }
+    else if (randomDrink == 4)
+    {
+        Drink.src = "../Images/Drinks/dvoinoLate.png";
+        Name.innerHTML = "Двойно лате";
+        Quantity.innerHTML = "500мл";
+    }
+    else if (randomDrink == 5)
+    {
+        Drink.src = "../Images/Drinks/Espreso.png";
+        Name.innerHTML = "Еспресо";
+        Quantity.innerHTML = "60мл / 120мл";
+    }
+    else if (randomDrink == 6)
+    {
+        Drink.src = "../Images/Drinks/espresoMakiato.png";
+        Name.innerHTML = "Еспресо макиато";
+        Quantity.innerHTML = "120мл";
+    }
+    else if (randomDrink == 7)
+    {
+        Drink.src = "../Images/Drinks/Frape.png";
+        Name.innerHTML = "Фрапе";
+        Quantity.innerHTML = "500мл";
+    }
+    else if (randomDrink == 8)
+    {
+        Drink.src = "../Images/Drinks/fredoKapuchino.png";
+        Name.innerHTML = "Фредо капучино";
+        Quantity.innerHTML = "400мл";
+    }
+    else if (randomDrink == 9)
+    {
+        Drink.src = "../Images/Drinks/Kakao.png";
+        Name.innerHTML = "Мляко с какао";
+        Quantity.innerHTML = "300мл";
+    }
+    else if (randomDrink == 10)
+    {
+        Drink.src = "../Images/Drinks/Kapuchino.png";
+        Name.innerHTML = "Капучино";
+        Quantity.innerHTML = "300мл";
+    }
+    else if (randomDrink == 11)
+    {
+        Drink.src = "../Images/Drinks/Late.png";
+        Name.innerHTML = "Лате";
+        Quantity.innerHTML = "300мл";
+    }
+    else if (randomDrink == 12)
+    {
+        Drink.src = "../Images/Drinks/mlqkoSNes.png";
+        Name.innerHTML = "Мляко с нес";
+        Quantity.innerHTML = "300мл";
+    }
+    else if (randomDrink == 13)
+    {
+        Drink.src = "../Images/Drinks/Neskafe.png";
+        Name.innerHTML = "Нескафе";
+        Quantity.innerHTML = "120мл";
+    }
+    else if (randomDrink == 14)
+    {
+        Drink.src = "../Images/Drinks/Sheik.png";
+        Name.innerHTML = "Шейк";
+        Quantity.innerHTML = "400мл";
+    }
+    else if (randomDrink == 15)
+    {
+        Drink.src = "../Images/Drinks/topalShokolad.png";
+        Name.innerHTML = 'Шoколад "Кафеварка"';
+        Quantity.innerHTML = "300мл";
+    }
+}
+
+function selectRandomDrinkCloseMenu()
+{
+    document.getElementById("selectedDrink").style.display = "none"
+}
+
 function getWidth(x)
 {
     if (x.matches)
